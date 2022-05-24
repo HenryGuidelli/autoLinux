4e9c7578fc69323589ca589bdf0344c628f7e7e9
HenryGuidelli <71869428+HenryGuidelli@users.noreply.github.com>
2022-05-24 09:09:39 -0300

    Add files via upload

---
+++ autoLinux/autoXampp-Installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "autoXampp 2.0"
+
+sudo wget https://www.apachefriends.org/xampp-files/8.1.6/xampp-linux-x64-8.1.6-0-installer.run
+
+sudo wgte https://github.com/HenryGuidelli/autoXampp/blob/main/autoXampp.sh
---
+++ autoLinux/autoXampp-Runner.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+echo "autoXampp"
+cd /opt/lampp/
+sudo ./manager-linux-x64.run
