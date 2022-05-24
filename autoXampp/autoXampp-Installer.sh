3f5ca66eca1b671858e6ff4404cd34645f9f0022
HenryGuidelli <71869428+HenryGuidelli@users.noreply.github.com>
2022-05-24 09:21:12 -0300

    Update autoXampp-Installer.sh

--- autoXampp/autoXampp-Installer.sh
+++ autoXampp/autoXampp-Installer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "autoXampp Installer"
-
 sudo wget https://www.apachefriends.org/xampp-files/8.1.6/xampp-linux-x64-8.1.6-0-installer.run
-
 sudo wgte https://github.com/HenryGuidelli/autoXampp/blob/main/autoXampp.sh
