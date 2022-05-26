beecd89c8290c4c5dcfe53305db9489025cef01c
HenryGuidelli <71869428+HenryGuidelli@users.noreply.github.com>
2022-05-25 22:10:08 -0300

    autoXampp-Installer 0.2

--- autoXampp/autoXampp-Installer.sh
+++ autoXampp/autoXampp-Installer.sh
@@ -1,4 +1,52 @@
 #!/bin/bash
 echo "autoXampp Installer"
-sudo wget https://www.apachefriends.org/xampp-files/8.1.6/xampp-linux-x64-8.1.6-0-installer.run
-sudo wgte https://github.com/HenryGuidelli/autoXampp/blob/main/autoXampp.sh
+
+if [ -e /opt/autoLinux/autoXampp ]; then
+	echo "PASTAS OK!"
+else
+	mkdir -p /opt/autoLinux/autoXampp
+fi
+cd //opt/
+sudo chmod 777 autoLinux/autoXampp/ -R
+cd /opt/autoLinux/autoXampp/
+
+
+if [ -e /opt/autoLinux/autoXampp/xampp-linux-installer.run ]; then
+	echo "PASTAS OK!"
+else
+	sudo wget -O xampp-linux-installer.run https://www.apachefriends.org/xampp-files/8.1.6/xampp-linux-x64-8.1.6-0-installer.run
+
+fi
+
+sudo chmod a+x xampp-linux-installer.run
+
+if [ -e /opt/lampp/ ]; then
+	echo "XAMPP OK!"
+else
+	sudo ./xampp-linux-installer.run
+fi
+
+cd /opt/lampp/ 
+sudo chmod 777 htdocs -R 
+
+if [ -e /opt/autoLinux/autoXampp/autoXampp-Runner.sh ]; then
+	echo "RUNNER OK!"
+else
+	sudo wget --convert-links -P /opt/autoLinux/autoXampp/ https://raw.githubusercontent.com/HenryGuidelli/autoLinux/main/autoXampp/autoXampp-Runner.sh
+fi
+
+if [ -e /opt/autoLinux/autoXampp/autoXampp-Runner.desktop ]; then
+	echo "APPLICATION-OPT OK!"
+else
+	sudo wget --convert-links -P /opt/autoLinux/autoXampp/ https://raw.githubusercontent.com/HenryGuidelli/autoLinux/main/autoXampp/autoXampp-Runner.desktop
+fi
+
+if [ -e /usr/share/applications/autoXampp-Runner.desktop ]; then
+	echo	"APPLICATION-USR OK!"
+else
+	cd /opt/autoLinux/autoXampp/
+	sudo cp autoXampp-Runner.desktop /usr/share/applications/
+fi
+
+cd //opt/
+sudo chmod 777 autoLinux/autoXampp/ -R
